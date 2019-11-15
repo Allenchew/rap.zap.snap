342e2f9944b185db556f40d6717db1a7589ffbc7
Allenchew <csoonzhi69@yahoo.com>
2019-11-15 13:03:49 +0900

    キャラ選択画面追加

---
+++ RapZapSnap/Assets/Chew/script/CharacterSelection.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CharacterSelection : MonoBehaviour
+{
+
+    [SerializeField]
+    public Vector3[] Presetposition= new Vector3[3];
+
+    public Image[] CharacterImages = new Image[3];
+
+  
+    
+    private bool moving_flag = false;
+    private Vector3 imagestepping;
+    enum movetoward
+    {
+        left=-1,
+        right=1
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        imagestepping = new Vector3(Mathf.Abs(Presetposition[1].x - Presetposition[0].x),0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (!moving_flag)
+            if (Input.GetKeyDown(pcontrol.leftb))
+            {
+                StartCoroutine(i_movepicture(movetoward.left));
+            }
+            else if (Input.GetKeyDown(pcontrol.rightb))
+            {
+                StartCoroutine(i_movepicture(movetoward.right));
+            }else if (Input.GetKeyDown(pcontrol.xb))
+            {
+                tset.Testing = true;
+            }*/
+    }
+    IEnumerator i_movepicture(movetoward toward)
+    {
+        moving_flag = true;
+        int tmpindex =0;
+        var tmppos = toward > 0 ? 0 : 2;
+        Vector3[] tmpimgpos = new Vector3[3];
+        float[] tmpalpha = new float[3];
+        for(int i = 0; i < 3; i++)
+        {
+            tmpalpha[i] = CharacterImages[i].color.a;
+            tmpimgpos[i] = CharacterImages[i].transform.localPosition;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if(Mathf.Abs(CharacterImages[i].transform.localPosition.x - Presetposition[tmppos].x) <= 0.02f)
+            {
+                Debug.Log("entered");
+                CharacterImages[i].transform.localPosition = Presetposition[2-tmppos];
+                tmpindex = i;
+            }
+        }
+        
+        for (float i=0f;i<1.02f;i+=0.02f)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if (j == tmpindex)
+                    continue;
+                CharacterImages[j].transform.localPosition = Vector3.Lerp(tmpimgpos[j], tmpimgpos[j] + ((int)toward * imagestepping),i);
+
+                if(tmpalpha[j]>0)
+                {
+                    CharacterImages[j].color = Color.Lerp(Color.white, Color.clear, i);
+                }
+                else
+                {
+                    CharacterImages[j].color = Color.Lerp(Color.clear, Color.white, i);
+                }
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+        moving_flag = false;
+        yield return null;
+    }
+   
+}
---
+++ RapZapSnap/Assets/Chew/script/CharacterSelectp1.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct Keybind
+{
+    public KeyCode leftb { get; set; }
+    public KeyCode rightb { get; set; }
+    public KeyCode upb { get; set; }
+    public KeyCode downb { get; set; }
+    public KeyCode tri { get; set; }
+    public KeyCode squ { get; set; }
+    public KeyCode ob { get; set; }
+    public KeyCode xb { get; set; }
+    public KeyCode enterb { get; set; }
+}
+
+public class CharacterSelectp1 : MonoBehaviour
+{
+    public GameObject[] images = new GameObject[6];
+
+    private bool f_movingslide = false;
+    private Keybind pcontrol = new Keybind();
+    private Vector3 tmppos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pcontrol = setupdefault(gameObject.tag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!f_movingslide)
+            if (Input.GetKeyDown(pcontrol.leftb))
+            {
+                StartCoroutine(i_sliding(images[1],tmppos,true));
+            }
+            /*else if (Input.GetKeyDown(pcontrol.rightb))
+            {
+                StartCoroutine(i_sliding());
+            }*/
+            else if (Input.GetKeyDown(pcontrol.xb))
+            {
+                tset.Testing = true;
+            }
+    }
+    IEnumerator i_sliding(GameObject target,Vector3 destination,bool fadeout)
+    {
+        f_movingslide = true;
+        Vector3 tmporgpos = target.transform.localPosition;
+        
+        for(float i = 0f; i < 1.02f; i += 0.02f)
+        {
+            if (fadeout)
+                target.GetComponent<Image>().color = Color.Lerp(Color.white, Color.clear, i);
+            target.transform.localPosition = Vector3.Lerp(tmporgpos, destination, i);
+            yield return new WaitForSeconds(0.01f);
+        }
+        target.transform.localPosition= Vector3.Lerp(tmporgpos, destination, 1f);
+        f_movingslide = false;
+    }
+    private Keybind setupdefault(string playernum)
+    {
+        Keybind controlkey = new Keybind();
+        if (playernum == "p1" || playernum == "p2")
+        {
+            controlkey.leftb = (playernum == "p1" ? KeyCode.A : KeyCode.LeftArrow);
+            controlkey.rightb = (playernum == "p1" ? KeyCode.D : KeyCode.RightArrow);
+            controlkey.upb = (playernum == "p1" ? KeyCode.W : KeyCode.UpArrow);
+            controlkey.downb = (playernum == "p1" ? KeyCode.S : KeyCode.DownArrow);
+            controlkey.tri = (playernum == "p1" ? KeyCode.I : KeyCode.Keypad8);
+            controlkey.squ = (playernum == "p1" ? KeyCode.J : KeyCode.Keypad4);
+            controlkey.ob = (playernum == "p1" ? KeyCode.L : KeyCode.Keypad6);
+            controlkey.xb = (playernum == "p1" ? KeyCode.K : KeyCode.Keypad5);
+            controlkey.enterb = (playernum == "p1" ? KeyCode.Return : KeyCode.KeypadEnter);
+        }
+        else
+        {
+            Debug.Log("playernum setting error");
+        }
+        Debug.Log(controlkey.leftb);
+        return controlkey;
+    }
+}
